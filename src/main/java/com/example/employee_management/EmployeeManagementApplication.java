0d7fb570e02fba4283b7123bdde40ad5221ee7f1
trungtq-0433 <49260658+trungtq-0433@users.noreply.github.com>
2026-02-21 23:55:30 +0700

    Module 1: Getting Started with Spring Boot (#1)

--- src/main/java/com/example/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/example/employee_management/EmployeeManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
-	public static void main(String[] args) {
+    static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
