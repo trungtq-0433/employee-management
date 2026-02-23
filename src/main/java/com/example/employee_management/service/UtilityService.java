830f79e76c997d228d31147eaaebf5ffc2b8561e
trungtq-0433 <tran.quang.trung@sun-asterisk.com>
2026-02-23 10:50:37 +0700

    Module 2: Custom Bean & IoC

---
+++ src/main/java/com/example/employee_management/service/UtilityService.java
@@ -0,0 +1,16 @@
+package com.example.employee_management.service;
+
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Service
+public class UtilityService {
+    public String generateEmployeeCode() {
+        String datePart = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String randomPart = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+
+        return "EMP-" + datePart + randomPart;
+    }
+}
